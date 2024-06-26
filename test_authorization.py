53da32c5f02e2c40eb91d360e0b8efa1b716180b
Nataly Isaeva <mamadany25@gmail.com>
2024-06-26 22:48:03 +0400

    add first test

--- test_authorization.py
+++ test_authorization.py
@@ -2,7 +2,7 @@
 from selene import browser, be, have
 
 browser.open('https://dev.byhelp.ru/authorization')
-browser.element('[id=rc-tabs-0-tab-password]').click()
-(browser.element('[id=authorization-by-password_email]').should(be.blank).type('karmatestirovschicy@yandex.ru'))
-(browser.element('[id=authorization-by-password_password]').should(be.blank).type('12345678').press_enter())
+browser.element('#rc-tabs-0-tab-password').click()
+(browser.element('#authorization-by-password_email').should(be.blank).type('karmatestirovschicy@yandex.ru'))
+(browser.element('#authorization-by-password_password').should(be.blank).type('12345678').press_enter())
 
